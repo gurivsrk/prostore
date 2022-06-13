869f7fe209546b404c5598bdbe7bce61638aacd7
Gursharan Singh <gursharan@vsrkcapital.com>
2022-06-13 11:39:01 +0530

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <h1>Welcome to prostore</h1>
+      </>
+      <header className="App-header">
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
