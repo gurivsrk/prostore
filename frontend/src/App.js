1cf7db8993005dfbe0e1da94bbf1922914ae479b
Gursharan Singh <gursharan@vsrkcapital.com>
2022-07-05 18:46:00 +0530

    latest update on 5-july

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
+import React from 'react'
 import Header from "./component/Header";
+// eslint-disable-next-line 
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 import Footer from "./component/Footer";
-import {Container} from "react-bootstrap"; 
+import { Container } from "react-bootstrap"; 
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-      
-    <>
+    <BrowserRouter>
       <Header />
-      <main className="py-3">
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
+          <main className="py-3">
+            <Container>
+              <Routes>
+                <Route  path="/" element={<HomeScreen />} />
+                <Route  path="/product/:id" element={<ProductScreen />} />
+              </Routes>
+            </Container>
+          </main>
       <Footer />
-    </>
-
+    </BrowserRouter> 
   );
 }
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import { Col, Row } from 'react-bootstrap'
 import Product from '../component/Product'
 import products from '../products'
@@ -7,10 +8,10 @@ import products from '../products'
 const HomeScreen = () => {
   return (
     <>
-        <Row>
+        <Row  className="justify-content-md-center">
             <h3>Lastest Product</h3>
             {products.map((product) => (
-                <Col sm={12} md={6} lg={4} xl={3}>
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product} />
                 </Col>
             ))}
